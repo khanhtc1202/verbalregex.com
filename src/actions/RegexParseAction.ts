54f969efae779d11e2ab40a07413a7105933bf3c
khanhub <khanh.tran@uzabase.com>
2018-04-14 03:50:38 +0900

    import verbal expressiom

--- src/actions/RegexParseAction.ts
+++ src/actions/RegexParseAction.ts
@@ -1,10 +1,14 @@
+var VerEx = require("verbal-expressions");
+
 export type RegexParseAction = {
     type: "PARSE",
     regexString: string
 };
 
 export const parse = (verbalRegex: string): RegexParseAction => {
-    const regexString: string = 'sample' + verbalRegex;
+    var regexParser = VerEx();
+
+    const regexString: string = regexParser.toString() + 'sample' + verbalRegex;
     return {
         type: "PARSE",
         regexString: regexString
