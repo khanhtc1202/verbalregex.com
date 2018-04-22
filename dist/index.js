6114d0e41778383ad363960c1c6a8a5f4a4de1d8
Nguyen Bach | Felix <bach.nguyen@rakuten.com>
2018-04-22 17:18:10 +0900

    add clear field

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,3 @@
-
-
 $("#compile").click(function(){
     verbalRegex = $("#verbal_regex").val();
     tester = VerEx();
@@ -19,9 +17,36 @@ $("#compile").click(function(){
 
         $("#regex").val(regexPart);
         $("#flag").val(flagPart);
+
+        var re = new RegExp(regexPart,flagPart);
+
+
+        $('#match_string').highlightWithinTextarea({
+            highlight: re,
+            className: 'pick-color'
+        });
     }
     catch(e){
         console.log("Error: " + e);
         alert("Error on converting verbal string to Regex");
     }
+})
+
+// $("#regex").onchange(function(){
+//
+// })
+
+$('#match_string').highlightWithinTextarea({
+    highlight: '',
+    className: 'pick-color'
+});
+
+$('#clear').click(function(){
+    $("#verbal_regex").val('');
+
+    $("#regex").val('');
+    $("#flag").val('');
+
+    $("#match_string").val('');
+    $('#match_string').highlightWithinTextarea('update');
 })
